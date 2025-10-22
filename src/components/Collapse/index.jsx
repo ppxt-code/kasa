e6e69a7a0b1f8f5b141f66b0f1d39edec784acd5
ppxt-code <xuantrang.phamphu@gmail.com>
2025-10-22 17:54:36 +0200

    réécriture de collapse avec 2 états open et !open (hook)

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,18 +1,20 @@
+import {useState} from 'react'
 import arrowUp from '../../assets/arrow_up.png'
 
 function Collapse({title, content}) {
+  const [open, setOpen] = useState(false)
     return (
                   <div className="detail">
-                    <input type="checkbox" className="detailToggle" />
                     <div className="detailTitle">
                       <p>{title}</p>
-                      <div className="detailArrow">
-                        <img src={arrowUp} alt="detail arrow" />
-                      </div>
+                      <label>
+                        <div className="detailArrow">
+                          <input type="checkbox" onChange={()=>setOpen(!open)} className="detailToggle" hidden/>
+                          <img src={arrowUp} alt="detail arrow" className={open ? "rotated" : ""}/>
+                        </div>
+                      </label>  
                     </div>
-                    <div className="detailContent">	
-                      {content}
-                    </div>
+                    {open && <div className="detailContent">{content}</div>}
                   </div>)
 }
 export default Collapse
