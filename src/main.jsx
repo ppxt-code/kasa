1e9121f025c31d05e44b86edf48c008537b341ff
ppxt-code <xuantrang.phamphu@gmail.com>
2025-10-22 15:57:04 +0200

    deplacement du code style.css dans styles.scss

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import LogementProvider from "./components/LogementProvider"
 
-import './css/style.css'
 import './scss/styles.scss'
 import "@fontsource/montserrat-alternates";
 import "@fontsource/montserrat-alternates/700.css"
